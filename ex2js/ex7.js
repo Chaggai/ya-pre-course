b396ea1ff76f835357a76750ad661ba172facea7
Chaggai <chaggai.thal+github@gmail.com>
2022-09-24 17:54:32 +0300

    Added suport for multy students check

--- ex2js/ex7.js
+++ ex2js/ex7.js
@@ -6,11 +6,19 @@ function Student(name, id, grades = []) {
   }
 }
 
-let chaggai = new Student('Chaggai', 111, [90, 98, 68]);
+let chaggai = new Student('Chaggai', 1, [90, 98, 68]);
+let pnina = new Student('Pnina', 2, [77, 92, 84]);
+let avishai = new Student('Avishai', 3, [91, 92, 99, 100, 83]);
 
-function main(student, minGrade) {
-  return student.grades.filter(grade => grade > minGrade);
+let students = [chaggai, pnina, avishai];
+
+function main(students, minGradeToDisplay) {
+  const grades = [];
+  students.forEach(student => {
+    student.grades.forEach(grade => grade > minGradeToDisplay ? grades.push(grade) : null)
+  });
+  return grades;
 }
 
-const result = main(chaggai, 80)
+const result = main(students, 80)
 console.log(...result);
