f5e22d78c8e0a0857e421e3cc4e9d8e2bdb6db7a
Chaggai <chaggai.thal+github@gmail.com>
2022-09-24 17:16:19 +0300

    Changed ex5 to be Avrg insted of Sum

--- ex2js/ex5.js
+++ ex2js/ex5.js
@@ -1,12 +1,14 @@
 const arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]; 
 
 function main(arr) {
-  arr = arr.flat();
-  arr = arr.reduce((acc, curr) => {
-    return acc += curr;
-  });
-  return `The total sum of inner numbers: ${arr}`;
-}
+  let counter = 0;
+  return arr.reduce((acc, curr) => {
+    return acc + curr.reduce((acc, curr, j) => {
+      counter += j;
+      return acc + curr;
+    });
+  }, 0) / counter;
+};
 
 const result = main(arr);
 console.log(result);
